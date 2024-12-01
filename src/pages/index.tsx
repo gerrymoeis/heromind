4484ad09cdf2bd19420cda940213f04e42334749
gerrymoeis <gerrymoeis@gmail.com>
2024-12-01 21:51:33 +0700

    First Commit - Setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Selamat Datang di Heromind</h1>
+    </div>
+  );
+};
+
+export default HomePage;
